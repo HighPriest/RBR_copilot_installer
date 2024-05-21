4155b53115fcbf8c2b75da108d0f57075b92f629
HighPriest <git_user@amun.pl>
2024-05-21 17:51:37 +0200

    Repo Init

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Pacenotes_Installer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
