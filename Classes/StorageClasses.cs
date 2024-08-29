416222fce07e9c99498ac422115aee8f299462ca
HighPriest <git_user@amun.pl>
2024-08-29 21:44:15 +0200

    [WIP] Create base for DownloadManager
    Note: Missing Secrets

---
+++ Classes/StorageClasses.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacenotes_Installer.Classes
+{
+    using System.Collections.Generic;
+    using System.Security.Cryptography.X509Certificates;
+
+    #region RbrConfigurationsStorage
+    internal class RbrConfigurationsStorage
+    {
+        public Configurations? Configurations = new();
+        public Languages? Languages = new();
+    }
+    public class Configuration
+    {
+        public string? Id;
+    }
+
+    public class Voice
+    {
+        public string? Id;
+    }
+    public class Language
+    {
+        public Dictionary<string, Voice>? Voices = [];
+    }
+
+    public class Configurations
+    {
+        public Dictionary<string, Configuration>? Configuration = [];
+    }
+
+    public class Languages
+    {
+        public Dictionary<string, Language>? Language = [];
+    }
+    #endregion RbrConfigurationsStorage
+
+    #region CrewChiefConfigurationsStorage
+    internal class CrewChiefConfigurationsStorage {
+        // TODO: ToImplement
+    }
+
+
+    #endregion CrewChiefConfigurationsStorage
+
+}
---
+++ DownloadManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using Pacenotes_Installer.Classes;
+
+namespace Pacenotes_Installer
+{
+    internal class DownloadManager
+    {
+        private static Supabase.Client supabase;
+        public DownloadManager()
+        {
+            string PublicURL = "";
+            string PublicKey = "";
+
+            InitializeSupabase(PublicURL, PublicKey);
+        }
+
+        public static void InitializeSupabase(string url, string key)
+        {
+            supabase.Realtime.Disconnect();
+            var options = new Supabase.SupabaseOptions
+            {
+                AutoConnectRealtime = true
+            };
+
+            supabase = new Supabase.Client(url, key, options);
+
+            try
+            {
+                Task.Run(() => supabase.InitializeAsync()).Wait();
+            }
+            catch
+            {
+                // set status to non-initialized
+            }
+        }
+
+        #region InstallationMethods
+        public void createRBRBackup(string directory)
+        {
+
+        }
+        public void restoreRBRBackup(string directory)
+        {
+
+        }
+        public void installFile(string destinationDir, string fileName)
+        {
+
+        }
+        #endregion InstallationMethods
+
+        #region DownloadMethods
+        public void ListAllFiles()
+        {
+
+        }
+
+        public void DownloadFile(string fileName) 
+        {
+
+        }
+
+        public void ExtractZipFile(byte[] zipFileBytes, string destinationDirectory)
+        {
+
+        }
+        #endregion DownloadMethods
+
+        #region ConfigurationMethods
+        public void CreateRBRConfiguration(string destinationDir)
+        {
+
+        }
+
+        public void ReadRBRConfiguration(string directory)
+        {
+
+        }
+
+        #endregion ConfigurationMethods
+    }
+}
